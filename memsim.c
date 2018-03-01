e4f3d79d3d10fb62bca0b28685b283f7b0567c4f
MMonnik <mmonnik@mail.usf.edu>
2018-03-01 15:50:20 -0500

    Updated memsim.c

--- memsim.c
+++ memsim.c
@@ -13,13 +13,30 @@ void lru();
 void fifo();
 void vms();
 
-int main(int argc, char *argv[], int argv, char argv[], char argv[]) { //requires <tracefile> <nframes> <lru|fifo|vms> <debug|quiet>
+int main(int argc, char *argv[], int argv[]) { //requires <tracefile> <nframes> <lru|fifo|vms> <debug|quiet>
 
+	char tracefile = argv[0];
+	int nframes = argv[1];
+	char alg = argv[2]; //lru/fifo/vms
+	char mode = argv[3]; //debug/quiet
+	
+	int TraceEvents = 0;
 	int tdr = 0; //total disk reads
 	int tdw = 0; //total disk writes
 
-	printf ("total memory frames: %d", ); //needs total memory frames
-	printf ("events in trace: %d", ); // needs events in trace
+	FILE *file;
+	file = fopen (filename, "r");
+	if (file) {
+		while (fscanf (file, "%s") != EOF) {
+			//does stuff
+		}
+		fclose (file)
+	}
+	
+	
+	//QUIET MODE
+	printf ("total memory frames: %d", nframes); //needs total memory frames
+	printf ("events in trace: %d", TraceEvents); // needs events in trace
 	printf ("total disk reads: %d", tdr);
 	printf ("total disk writes: %d", tdw);
 
