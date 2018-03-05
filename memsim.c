6cbf57d0a5e27b9fafa2a684c41e1732c1260a58
MMonnik <mmonnik@mail.usf.edu>
2018-03-04 23:15:03 -0500

    Updated scan and functions

--- memsim.c
+++ memsim.c
@@ -10,8 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void lru();
-void fifo();
+void lru(int time[], int n);
+void fifo(int *q, int x);
 void vms();
 
 int main(int argc, char *argv[], int argv[]) { //requires <tracefile> <nframes> <lru|fifo|vms> <debug|quiet>
@@ -25,13 +25,17 @@ int main(int argc, char *argv[], int argv[]) { //requires <tracefile> <nframes> 
 	int tdr = 0; //total disk reads
 	int tdw = 0; //total disk writes
 	char action; //action records either R for read or W for write in the trace
+	char str[9] //8 letter string + null
+	
+	
 
 	//DEBUG MODE
 	FILE *file;
 	file = fopen (filename, "r");
 	if (file) {
 		while (fscanf (file, "%s") != EOF) {
-			//does stuff
+			fscanf (file, "%s % c", str, action); //reads string '0129abcf W', store in str
+			
 		}
 		fclose (file)
 	}
@@ -47,12 +51,50 @@ int main(int argc, char *argv[], int argv[]) { //requires <tracefile> <nframes> 
 
 }
 
-void lru() {
-
+void lru(int time[], int n) {
+	int i, minimum = time[0], pos = 0;
+ 
+    for(i = 1; i < n; ++i){
+        if(time[i] < minimum){
+            minimum = time[i];
+            pos = i;
+        }
+    }
+    
+    return pos;
 }
 
-void fifo() { //queue
-
+void fifo(int *q, int x) { //queue
+	int head = 0;
+	int tail = 0;
+	
+	//printf("head=%d\ttail=%d\n", head, tail+1);
+	//enqueue
+	q[tail] = x;
+	if (tail == N) {      
+        	tail = 0;
+	}
+	else {
+        	tail++;
+	}
+    	//printf("tail=%d\n", tail);
+	
+	//dequeue
+	int x;
+    	//if (head == tail) {
+        //fprintf(stderr, "empty\n"); }
+   	 x = q[head];
+    	if (head == N) { 
+        	head = 0;
+	}
+	else { 
+        	head++;
+	}
+	return x;
+	int qempty(void) {
+    		return head == tail;
+	}
+	
 }
 
 void vms() {
